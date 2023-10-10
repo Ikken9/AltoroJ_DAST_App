cc136eb39ccbdb32ea7c97a2937e1900a5a594e9
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-10-10 00:01:56 -0300

    Ready version 2

--- xss_tester.py
+++ xss_tester.py
@@ -4,8 +4,6 @@ import requests
 def test(url, payload):
     for data in payload:
         url = url + "?query=" + data
-        print(data)
-        print(url)
         res = requests.get(url=url)
         if data in res.text:
             return 1
